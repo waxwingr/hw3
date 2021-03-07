36c73dcceded361d696f1a915a2a4c748bcb6cb9
waxwingr <77216412+waxwingr@users.noreply.github.com>
2021-03-08 01:31:21 +0200

    Add files via upload

---
+++ hw3_7.py
@@ -0,0 +1,7 @@
+california={1,2,3}
+philadelphia={2,3,5}
+
+print(california.intersection(philadelphia))
+print(california.union(philadelphia))
+print(california.difference(philadelphia))
+print(california.symmetric_difference(philadelphia))
---
+++ main_program3_5.py
@@ -0,0 +1,6 @@
+number1=20
+number2=30
+print(number1+number2)
+print(number1*number2)
+print((number1-number2)*(-1))
+print(number1/number2)
